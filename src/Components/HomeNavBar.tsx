7f6479f53c2eb1dde9684efcc6404dd8cfcdc257
yeray1422 <yeray14sj@gmail.com>
2022-07-11 15:37:41 +0200

    Added component HomeNavBar

---
+++ src/Components/HomeNavBar.tsx
@@ -0,0 +1,44 @@
+import { Search } from "@mui/icons-material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, IconButton, MenuItem, Stack, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+const HomeNavBar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+    const handleOpenMenu: () => void = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+  
+    const handleSearchIconClick: () => void = () => {
+      // TODO: Add functionality
+      console.log("Search button click");
+    };
+
+    return (
+        <AppBar position="static">
+        <Stack>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Zombies Easter Eggs
+            </Typography>
+            <IconButton onClick={handleSearchIconClick} className="search-icon">
+              <Search />
+            </IconButton>
+            <IconButton onClick={handleOpenMenu} className="menu-icon">
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+          <div className={isMenuOpen ? "menu-item-list" : "hide-menu-item-list"}>
+            <MenuItem>Black Ops I</MenuItem> {/* onClick */}
+            <MenuItem>Black Ops II</MenuItem> {/* onClick */}
+            <MenuItem>Black Ops III</MenuItem> {/* onClick */}
+            <MenuItem>Black Ops IV</MenuItem> {/* onClick */}
+            <MenuItem>Cold War</MenuItem> {/* onClick */}
+          </div>
+        </Stack>
+      </AppBar>
+    );
+}
+
+export default HomeNavBar
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,44 +1,10 @@
-import { AppBar, Toolbar, Typography, Stack, MenuItem, IconButton } from "@mui/material";
-import React, { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Search } from "@mui/icons-material";
+import React from "react";
+import HomeNavBar from "../Components/HomeNavBar";
 
 const Home = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleOpenMenu: () => void = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const handleSearchIconClick: () => void = () => {
-    // TODO: Add functionality
-    console.log("Search button click");
-  };
-
   return (
     <>
-      <AppBar position="static">
-        <Stack>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Zombies Easter Eggs
-            </Typography>
-            <IconButton onClick={handleSearchIconClick} className="search-icon">
-              <Search />
-            </IconButton>
-            <IconButton onClick={handleOpenMenu} className="menu-icon">
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-          <div className={isMenuOpen ? "menu-item-list" : "hide-menu-item-list"}>
-            <MenuItem>Black Ops I</MenuItem> {/* onClick */}
-            <MenuItem>Black Ops II</MenuItem> {/* onClick */}
-            <MenuItem>Black Ops III</MenuItem> {/* onClick */}
-            <MenuItem>Black Ops IV</MenuItem> {/* onClick */}
-            <MenuItem>Cold War</MenuItem> {/* onClick */}
-          </div>
-        </Stack>
-      </AppBar>
+      <HomeNavBar />
     </>
   );
 };
