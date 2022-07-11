e97a50d3337cb28d552581b6e7a9478a086591e9
yeray1422 <yeray14sj@gmail.com>
2022-07-11 16:11:16 +0200

    Added logo

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import HomeNavBar from "../Components/HomeNavBar";
+import HomeLogo from "../Assets/images/home-page/zombies-logo.png";
+import { Stack } from "@mui/material";
 
 const Home = () => {
   return (
     <>
       <HomeNavBar />
+      <Stack justifyContent="center" alignItems="center">
+        <img src={HomeLogo} alt="" className="home-logo" />
+        <div className="home-subtitle">
+          <p>La guía para completar los Easter Eggs de Call Of Duty Zombies</p>
+        </div>
+      </Stack>
     </>
   );
 };
