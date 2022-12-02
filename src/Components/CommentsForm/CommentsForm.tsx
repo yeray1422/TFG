f432924bbeb8d5b6e693be39ae487782f05afb90
yeray1422 <yeray14sj@gmail.com>
2022-12-02 09:43:35 +0100

    finally installed uuid

--- src/Components/CommentsForm/CommentsForm.tsx
+++ src/Components/CommentsForm/CommentsForm.tsx
@@ -7,6 +7,7 @@ import {
   Typography,
 } from "@mui/material";
 import React, { FormEvent, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const CommentsForm = () => {
   const [name, setName] = useState("");
